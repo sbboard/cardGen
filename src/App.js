1a4f6d09044eb385767a187172141fb859efa7ef
sbboard <colin.buffum@gmail.com>
2021-02-17 19:39:54 -0700

    experimenting with states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import Uploader from "./Home/Uploader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 5,
+  };
+
+  plusOne = () => {
+    this.setState({counter: this.state.counter + 1})
+  };
+
+  render() {
+    let peenDog = "peen";
+
+    return (
+      <div className="App">
+        <h1>
+          Hi, {peenDog}. {this.state.counter}
+        </h1>
+        <Uploader name="test" />
+        <button onClick={this.plusOne}></button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Home/Uploader.js
@@ -0,0 +1,6 @@
+const Uploader = (props) => {
+  let peen = "peendog"
+  return <div>one big {props.name + 5 + peen}</div>;
+};
+
+export default Uploader;
