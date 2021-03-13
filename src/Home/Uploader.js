3f0e41c0d4955475975639b5c1efcee6fe846352
sbboard <colin.buffum@gmail.com>
2021-03-12 19:08:29 -0700

    learned about react hooks

--- src/Home/Uploader.js
+++ src/Home/Uploader.js
@@ -1,6 +1,12 @@
-const Uploader = (props) => {
+import { useState } from "react";
+
+const Uploader = props => {
+  const bigManState = useState('cool');
+  
+console.log(bigManState)
+
   let peen = "peendog"
-  return <div>one big {props.name + 5 + peen}</div>;
+  return <div>one big {props.name + 5 + peen} and ofc {bigManState}</div>;
 };
 
 export default Uploader;
